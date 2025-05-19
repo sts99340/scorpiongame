fee66430b602c8581899207d4f2b31115e37ebf8
sts99340 <123031585+sts99340@users.noreply.github.com>
2025-05-19 01:00:37 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,8 +1,6 @@
 // Get the canvas and its context
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = "red";
-ctx.fillRect(10, 10, 100, 100);
 
 // Define the scorpion object
 const scorpion1 = {
